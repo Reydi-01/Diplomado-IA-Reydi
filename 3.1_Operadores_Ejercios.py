093ea99aa93ff8d5e52e549774fa38e0a4a23aad
Reydi-01 <reydipalacios@gmail.com>
2024-06-18 23:07:55 -0600

    Ejercicios de la segunda sesión

---
+++ 3.1_Operadores_Ejercios.py
@@ -0,0 +1,18 @@
+""""
+Escribir un programa que solicite dos números y luego imprima:
+    1. La suma de los dos números
+    2. La resta del primer número menos el segundo
+    3. El producto de los dos números
+    4. El cubo de la suma de los dos números
+    5. El cociente de la división del primer número por el segundo
+
+"""
+# Este es mi primer código
+num1 = float(input("ingrese el primer número:"))
+num2 = float(input("ingrese el segundo número"))
+
+print(num1 + num2)
+print( num1 - num2)
+print(num1 * num2)
+print((num1 + num2)**3)
+print( num1 / num2)
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Ciencia de datos e inteligencia artificial en la agricultura
 
 ## Modulo 1
+Introduccion a la programacion 
